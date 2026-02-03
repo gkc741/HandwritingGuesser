a59c43e35f494ffb47563c8e191da2bbfbf7742e
Thomas Juel Robertson <thomasrobertson@outlook.dk>
2026-02-04 00:33:38 +0100

    Added nn_layers
    Completley rewrote the whole thing to use flattend row-major list instead of 2d arrays and uses layers instead of neurons and networks. Also uses bin files instead of csv. Also made it so i can test it on files instead of just training it. also no .h file because i cant be asked

---
+++ nn_layers.c
@@ -0,0 +1,443 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+typedef struct layer{
+    float* weights; // Long flattended list of weights for every neruon in the layer
+    float* bias; // the bias
+    int nr_of_neurons;  // the size of the out list basically how many neurons
+    int in_size; // the size of the input list
+}layer;
+/*
+The weight list is a flattended list so that means its a 2d array in flattended into row-major 1d list
+this means i acces it instead of [i][j] i do, [i * (weight pr neuron / in_size) + j]
+
+The bias is just a long list of all the biases of the neurons
+*/
+
+
+// I want to create a layer with random weights and bias
+struct layer create_ran_layer(int nr_of_neurons, int nr_of_inputs){
+    struct layer n;
+    n.nr_of_neurons = nr_of_neurons;
+    n.in_size = nr_of_inputs;
+
+    // i want a random bias for every neuron
+    n.bias = malloc(nr_of_neurons * sizeof(float));
+    for(int i = 0; i < nr_of_neurons; i++){
+        n.bias[i] = ((float)rand() / RAND_MAX) * 0.002 - 0.001;  // random biasa between [-0.001, 0.001]
+    }
+    
+    // i want in_size weights pr neuron so i need the input size they are going to take
+    n.weights = malloc(nr_of_inputs * nr_of_neurons * sizeof(float));
+    for(int i = 0; i < nr_of_inputs * nr_of_neurons; i++){
+        n.weights[i] = ((float)rand() / RAND_MAX) * 0.02 - 0.01;  // random weight between [-0.01, 0.01]
+    }
+    return n;
+}
+
+
+// Free it ofc
+void free_layer(struct layer layer){
+    free(layer.bias);
+    free(layer.weights);
+}
+
+
+
+static uint32_t read_be_u32(FILE *f){
+    uint8_t bytes[4];
+    if(fread(bytes, 1, 4, f) != 4){
+        fprintf(stderr, "Failed to read header bytes\n");
+        exit(EXIT_FAILURE);
+    };
+    // CONVERT BIG EDIAN TO SMALL EDIAN
+    return ((uint32_t)bytes[0] << 24) | ((uint32_t)bytes[1] << 16) | ((uint32_t)bytes[2] << 8) | (uint32_t)bytes[3];
+}
+
+
+typedef struct dataset{
+    float* data;  // flattend list of datapoint (pixel values)
+    int data_size;  // rows * colms (so the number of pixels)
+    uint8_t* labels;  // LIST OF THE ACTAUAL VALUE OF THE PIC
+    int nr_of_images; 
+}dataset;
+
+
+struct dataset read_images(const char* file){
+    // in the MNIST dataset the first 4 things are 32bit intigeres in big edian order so i need to handle them seperatley
+    // then the rest should be uint_8 so i read them to a very long list of size colm * row 
+    // i should use mmap but for now im using fread
+
+    FILE* f = fopen(file, "rb");
+    if(!f){
+        fprintf(stderr, "Could not open %s\n", file);
+        exit(EXIT_FAILURE);
+    }
+
+    // READ/PRINT THE HEADER
+    uint32_t magic = read_be_u32(f);
+    uint32_t images = read_be_u32(f);
+    uint32_t rows = read_be_u32(f);
+    uint32_t cols = read_be_u32(f);
+    printf("magic: %u\nimages: %u\nrows: %u\ncols: %u\n", magic, images, rows, cols);
+
+    // CONSTRUCT THE DATASET
+    struct dataset ds = {NULL, rows * cols, NULL, images};
+    ds.data = malloc(sizeof(float) * rows * cols * images);
+    uint8_t* data = malloc(sizeof(uint8_t) * rows * cols * images);
+
+    // READ VALUES
+    uint32_t value = 0;
+    value = fread(data, sizeof(uint8_t), rows * cols * images, f);
+    printf("read %u points of data\n", value);
+    assert(value == images * rows * cols);
+    
+    
+    // NORMALIZE THE VALUES
+    for(uint32_t i = 0; i < value; i++){
+        ds.data[i] = (float)data[i] / 255.0;
+    }
+
+    free(data);
+    fclose(f);
+    return ds;
+}
+
+uint8_t* read_labels(const char* file){
+    FILE* f = fopen(file, "rb");
+    if(!f){
+        fprintf(stderr, "Could not open %s\n", file);
+        exit(EXIT_FAILURE);
+    }
+
+    // READ/PRINT THE HEADER
+    uint32_t magic = read_be_u32(f);
+    uint32_t images = read_be_u32(f);
+    printf("magic: %u\nimages: %u\n", magic, images);
+
+    // INITIALIZE 
+    uint8_t* data = malloc(sizeof(uint8_t) * images);
+    uint32_t value = 0;
+
+    // READ VALUES
+    value = fread(data, sizeof(uint8_t), images, f);
+    printf("read %u img labels\n", value);
+    assert(value == images);
+
+    fclose(f);
+    return data;
+}
+
+
+struct dataset read_data(const char* image_file, const char* label_file){
+    struct dataset ds = read_images(image_file);
+    ds.labels = read_labels(label_file);
+    return ds;
+}
+
+
+void free_dataset(struct dataset dataset){
+    free(dataset.data);
+}
+
+
+
+// NOW IVE CONSTRUCTED THE LAYERS AND THEN DATA READING TIME FOR THE MATH
+
+inline float relu(float x){  // INLINE MAKES IT SO WHEN I CALL IT IT JUST IMPUTS THE MATH IN WHERE I CALL IT
+    return x > 0 ? x : 0;   // IS X > 0 IF YES RETURN X ELSE RETURN 0 (CALLED TENARY OPERATOR)
+}
+
+
+
+typedef struct {
+    float* output_list;
+    int size;
+}output;
+
+
+output calc_hidden_layer_output(struct dataset dataset, struct layer hidden_layer, int img_indx){
+    output output;
+    output.output_list = malloc(sizeof(float) * hidden_layer.nr_of_neurons);
+    output.size = hidden_layer.nr_of_neurons;
+
+    float temp;
+    // I NEED TO TIMES THE INPUT BY THE WEIGHTS
+    // BUT THE INPUT IS 784 LONG AND THE WEIGHTS IS 784 * NR_OF_NEURONS
+    // I NEED FOR EVERY INPUT TIMES IT BY WEIGHTS AND THEN MOVE WEIGHTS 784 WHICH IS THE LENGTH OF ONE NEURONS WEIGHTS
+    float* image_data = &dataset.data[img_indx * dataset.data_size];
+    for(int j = 0; j < hidden_layer.nr_of_neurons; j++){ // so for every neuron
+        temp = hidden_layer.bias[j];
+        for(int i = 0; i < hidden_layer.in_size; i++){  // in size should be the dataset row * cols
+            // basically for every dataset item for every neuron
+            temp += hidden_layer.weights[j * hidden_layer.in_size + i] * image_data[i];
+        }
+        output.output_list[j] = relu(temp);
+    }
+    return output;
+}
+
+
+output calc_output_layer_output(float* output_of_hidden_layer, struct layer output_layer){
+    output output = {NULL, output_layer.nr_of_neurons};
+    output.output_list = malloc(sizeof(float) * output_layer.nr_of_neurons);
+
+    float temp;
+    // I NEED TO SAY OUTPUT = INPUT(WHICH IS OUTPUT OF HIDDEN LAYER) @ WEIGHTS + BIAS
+    for(int i = 0; i < output_layer.nr_of_neurons; i++){
+        temp = output_layer.bias[i];
+        for(int j = 0; j < output_layer.in_size; j++){
+            temp += output_layer.weights[i * output_layer.in_size + j] * output_of_hidden_layer[j];
+        }
+        output.output_list[i] = temp;
+    }
+    return output;
+}
+
+// TURN THE OUTPUT FROM OUTPUT LAYER INTO PROBABILITIES USING SOFTMAX
+output calc_softmax_prob(output* output_of_output_layer){
+    output z = {NULL, output_of_output_layer->size};
+    z.output_list = malloc(sizeof(float) * output_of_output_layer->size);
+    // FIRST WE FIND THE LATRGEST VALUE
+    float max = output_of_output_layer->output_list[0];
+    for(int i = 0; i < output_of_output_layer->size; i++){
+        z.output_list[i] = output_of_output_layer->output_list[i];
+        if (z.output_list[i] > max){
+            max = z.output_list[i];
+        }
+    }
+
+    // THEN WE EXPONENTIATE AND MINUS THE MAX VALUE AND SUM IT
+    float sum_exp = 0.0;
+    for(int i = 0; i < output_of_output_layer->size; i++){
+        z.output_list[i] = exp(z.output_list[i] - max);
+        sum_exp += z.output_list[i];
+    }
+
+
+    // THEN WE DEVVIDE BY THE SUM TO GET THE PROBABILITY
+    for(int i = 0; i < output_of_output_layer->size; i++){
+        z.output_list[i] /= sum_exp;
+    }
+
+    return z;    // THIS IS A LIST OF 10 WITH THE PROBABILITY OF EACH BEING THE ONE
+}
+
+
+
+
+float calc_loss(dataset dataset, float* predictioin_probabilities, int img_indx){
+    int label = dataset.labels[img_indx];
+    float loss = -log(predictioin_probabilities[label]);  // -log of the probability it got for the correct digit
+    return loss;
+}
+
+
+int actual_prediction(output prob_prediction_list){
+    // FIND THE PREDICTID CLASS
+    int pred_class = 0;
+    float max_prob = prob_prediction_list.output_list[0];
+    for(int i = 0; i < prob_prediction_list.size; i++){
+        if(prob_prediction_list.output_list[i] > max_prob){
+            max_prob = prob_prediction_list.output_list[i];
+            pred_class = i;
+        }
+    }
+    return pred_class;
+}
+
+float* output_layer_errors(dataset dataset, output prob_prediction, int img_indx){
+    int label = dataset.labels[img_indx];
+
+    float* error_list_of_output = malloc(sizeof(float) * prob_prediction.size);
+    
+    for(int i = 0; i < prob_prediction.size; i++){
+        float t = (i == label) ? 1.0 : 0.0;
+        error_list_of_output[i] = prob_prediction.output_list[i] - t;
+    }
+    return error_list_of_output;
+}
+
+
+float* hidden_layer_errors(output hidden_layer_output, layer output_layer, float* error_list_of_output){
+    int size = hidden_layer_output.size;
+    float* error_list_of_hidden = malloc(sizeof(float) * size);
+
+    for(int i = 0; i < size; i++){
+        if(hidden_layer_output.output_list[i] <= 0){
+            error_list_of_hidden[i] = 0;
+            continue;
+        }
+        float sum = 0;
+        for(int j = 0; j < output_layer.nr_of_neurons; j++){
+            sum += error_list_of_output[j] * output_layer.weights[j * output_layer.in_size + i];
+        // BAD SPATIAL LOCALITY BUT WITH EARLY CONTINUE AND SPARSE RELU ITS WORTH
+        }
+        error_list_of_hidden[i] = sum;
+    }
+    return error_list_of_hidden;
+}
+
+
+
+
+void update_output_weights_and_bias(layer* output_layer, float* error_list_of_output, float eta, output hidden_layer_output){
+    // for every neuron (10)
+    for(int i = 0; i < output_layer->nr_of_neurons; i++){
+        // for every weight of every neuron (100)
+        for(int j = 0; j < output_layer->in_size; j++){
+            output_layer->weights[i * output_layer->in_size + j] -= eta * error_list_of_output[i] * hidden_layer_output.output_list[j];
+        }
+        output_layer->bias[i] -= eta * error_list_of_output[i];
+    }
+    return;
+}
+
+
+
+
+void update_hidden_weights_and_bias(layer* hidden_layer, float* error_list_of_hidden, dataset dataset, float eta, int img_indx){
+    float* input_values = &dataset.data[img_indx * dataset.data_size]; // pointer to the element and then you just get the next one cuz of malloc
+
+    for(int i = 0; i < hidden_layer->nr_of_neurons; i++){
+        for(int j = 0; j < hidden_layer->in_size; j++){
+            hidden_layer->weights[i * hidden_layer->in_size + j] -= eta * error_list_of_hidden[i] * input_values[j];
+        }
+        hidden_layer->bias[i] -= eta * error_list_of_hidden[i];
+    }
+    return;
+}
+
+
+float train_NN(dataset dataset, layer* hidden_layer, layer* output_layer, float eta, int batch_size){
+    float accuracy = 0.0;
+
+    // ACCUMILATE ERRORS FOR BATCH TRAINING
+    float* accumilated_error_output = malloc(sizeof(float) * output_layer->nr_of_neurons);
+    float* accumilated_error_hidden = malloc(sizeof(float) * hidden_layer->nr_of_neurons);
+
+    for(int j = 0; j < output_layer->nr_of_neurons; j++) accumilated_error_output[j] = 0;
+    for(int j = 0; j < hidden_layer->nr_of_neurons; j++) accumilated_error_hidden[j] = 0;
+
+    for(int i = 0; i < dataset.nr_of_images; i++){
+        // FORWARD
+        output hidden_output = calc_hidden_layer_output(dataset, *hidden_layer, i);
+    
+        output output_output = calc_output_layer_output(hidden_output.output_list, *output_layer);
+    
+        output probs = calc_softmax_prob(&output_output);
+    
+        int prediction = actual_prediction(probs);
+
+
+        // BACKWARD
+    
+    
+        // I NEED TO CALC ERRORS EVERY TIME 
+        // calc errors
+        float* error_output = output_layer_errors(dataset, probs, i);
+        float* error_hidden = hidden_layer_errors(hidden_output, *output_layer, error_output);
+        
+        // assign to accumilated
+        for(int j = 0; j < output_layer->nr_of_neurons; j++){
+            accumilated_error_output[j] += error_output[j];
+        }
+
+        for(int j = 0; j < hidden_layer->nr_of_neurons; j++){
+            accumilated_error_hidden[j] += error_hidden[j];
+        }
+
+        if((i + 1) % batch_size == 0){
+            // average them
+            for(int j = 0; j < output_layer->nr_of_neurons; j++){
+                accumilated_error_output[j] /= batch_size; 
+            }   
+        
+            for(int j = 0; j < hidden_layer->nr_of_neurons; j++){
+                accumilated_error_hidden[j] /= batch_size;  
+            }
+            
+            // update weights
+            update_hidden_weights_and_bias(hidden_layer, accumilated_error_hidden, dataset, eta, i);
+            update_output_weights_and_bias(output_layer, accumilated_error_output, eta, hidden_output);
+
+            for(int j = 0; j < output_layer->nr_of_neurons; j++) accumilated_error_output[j] = 0;
+            for(int j = 0; j < hidden_layer->nr_of_neurons; j++) accumilated_error_hidden[j] = 0;
+        }
+
+        free(hidden_output.output_list);
+        free(output_output.output_list);
+        free(probs.output_list);
+        free(error_hidden);
+        free(error_output);
+
+        if(prediction == dataset.labels[i]) accuracy++;
+    }
+    accuracy =(100.0 * accuracy) / dataset.nr_of_images;
+    return accuracy;
+}
+
+float test_on_data(dataset dataset, layer* hidden_layer, layer* output_layer){
+    float accuracy = 0.0;
+    for(int i = 0; i < dataset.nr_of_images; i++){
+        output hidden_output = calc_hidden_layer_output(dataset, *hidden_layer, i);
+    
+        output output_output = calc_output_layer_output(hidden_output.output_list, *output_layer);
+    
+        output probs = calc_softmax_prob(&output_output);
+        
+        int prediction = actual_prediction(probs);
+        if(prediction == dataset.labels[i]) accuracy++;
+    }
+    accuracy =(100.0 * accuracy) / dataset.nr_of_images;
+    return accuracy;
+}
+
+
+
+int main(){
+    srand(13);
+
+    printf("\n==== LOADING TRAINING DATA ====\n");
+    dataset ds = read_data("archive/bin_data/train/train-images.idx3-ubyte", "archive/bin_data/train/train-labels.idx1-ubyte");
+    
+    printf("\n==== LOADING TEST DATA ====\n");
+    dataset test_data = read_data("archive/bin_data/test/t10k-images.idx3-ubyte", "archive/bin_data/test/t10k-labels.idx1-ubyte");
+
+    int nr_of_neurons = 100;
+    int nr_of_input = ds.data_size;
+
+    layer hidden_layer = create_ran_layer(nr_of_neurons, nr_of_input);
+    layer output_layer = create_ran_layer(10, hidden_layer.nr_of_neurons);
+
+    float eta = 0.01;
+
+    int batch_size = 1;
+    // KEEP BATCH AT 1 BECAUSE update_hidden_weights_and_bias DOSENT WORK WITH THE i INPUT PROPERRLY
+    
+    int epochs = 2;
+    // after 2 i get overfitting
+
+    printf("\n==== TRAINING AND TESTING====\n");
+    for(int run = 0; run < epochs; run++){
+        float accuracy_train = train_NN(ds, &hidden_layer, &output_layer, eta, batch_size);
+        float accuracy = test_on_data(test_data, &hidden_layer, &output_layer);
+        // printf("Accuracy: %.2f%%\n", accuracy_train);
+        // printf("accyracy test: %.2f%%\n\n", accuracy);
+        printf("epoch %d: train=%.2f%%, test=%.2f%%, diff=%.2f\n", run + 1, accuracy_train, accuracy, accuracy_train - accuracy);
+    }
+    
+    
+    
+    free_layer(hidden_layer);
+    free_layer(output_layer);
+    free_dataset(ds);
+    free_dataset(test_data);
+
+    return 0;
+}
